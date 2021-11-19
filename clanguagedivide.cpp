a4406e1471b57a0f98280f97b5d033ebe557c848
koradajayaram2003 <93367843+koradajayaram2003@users.noreply.github.com>
2021-11-19 12:47:19 +0530

    Add files via upload

---
+++ clanguagedivide.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{int a,b,result;
+printf(" Enter your first number\n");
+scanf("%d",&a);
+printf("Enter your second number\n");
+scanf("%d" ,&b);
+result=a*b;
+printf("the multiply of two numbers is:\n%d" ,result);
+result=a+b;
+printf("\nThe sum of two numbers is:\n%d" ,result);
+result=a/b;
+printf("\nThe division of two numbers is:\n%d" ,result);
+}
+
